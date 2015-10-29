4cf76c7d2ed1adc1bf696d8a077028e115537dd7
xiaoli <fenglingne@gmail.com>
2015-10-29 13:22:41 +0800

    use dev branch to commit

--- src/main/java/com/xiaoli/study/util/DateUtil.java
+++ src/main/java/com/xiaoli/study/util/DateUtil.java
@@ -1,11 +1,11 @@
 package com.xiaoli.study.util;
 
 import java.util.Date;
-
 public class DateUtil {
 
 	Date d1 = new Date();
 	Date d2 = new Date();
 	Date d3 = new Date();
+	Date d4 = null;
 	
 }
