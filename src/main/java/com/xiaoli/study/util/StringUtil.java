9b428be1c704235218e6f03917aac391c5deeab8
xiaoli <fenglingne@gmail.com>
2015-10-30 13:48:54 +0800

    add log4j slf4j config

--- src/main/java/com/xiaoli/study/util/StringUtil.java
+++ src/main/java/com/xiaoli/study/util/StringUtil.java
@@ -1,7 +1,17 @@
 package com.xiaoli.study.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StringUtil {
 	
-	
+	static Logger LOGGER = LoggerFactory.getLogger(StringUtil.class);
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            if (i % 2 == 0)
+                LOGGER.info("Hello {}", i);
+            else
+                LOGGER.info("I am on index {}", i);
+    }
 
 }
